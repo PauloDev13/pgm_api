a1a9e3c1151a1c625c045a6cbd11f7159fe7e9a4
Paulo Roberto De Morais <prmorais1302@gmail.com>
2026-03-05 12:45:12 -0300

    Initial project

---
+++ src/main/java/br/gov/rn/pgmapi/PgmApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.rn.pgmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgmApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/gov/rn/pgmapi/PgmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.rn.pgmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
